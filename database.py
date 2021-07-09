340355727feb2033f4353a2c8ef83bc17cb85236
WilliamW5 <WilliamWiemann@outlook.com>
2021-07-08 21:55:06 -0400

    ade

--- database.py
+++ database.py
@@ -5,9 +5,9 @@ import sqlite3
 root = Tk()
 root.title('Database')
 root.iconbitmap('Images/WillRagB.ico')
-root.geometry('800x600')
+root.geometry('375x300')
 
-# Database 
+# Database
 
 # Create a Database or connect to an existing one
 conn = sqlite3.connect('address_book.db')
@@ -15,24 +15,42 @@ conn = sqlite3.connect('address_book.db')
 # Create Cursor: cursor executes commands and comes back with result
 curs = conn.cursor()
 
-# Create table: 
+# Create table:
 # datatypes in sqlite3: text, integers(whole), real(decimal), null, blob(image/video files)
-
-#curs.execute("""CREATE TABLE addresses (
+# curs.execute("""CREATE TABLE addresses (
 #    first_name text,
 #    last_name text,
+#    address text,
 #    city text,
 #    state text,
 #    zipcode integer
 #    )""")
 
 # Create Submit Function for database
+
+
 def submit():
     # Connect to the database
     conn = sqlite3.connect('address_book.db')
     # Create cursor for function
     curs = conn.cursor()
-    
+
+    # Insert into Table
+    curs.execute("INSERT INTO addresses VALUES (:f_name, :l_name, :address, :city, :state, :zipcode)",
+                 {
+                     'f_name': f_name.get(),
+                     'l_name': l_name.get(),
+                     'address': address.get(),
+                     'city': city.get(),
+                     'state': state.get(),
+                     'zipcode': zipcode.get()
+                 })
+
+    # Commit changes: after changes you want to commit
+    conn.commit()
+
+    # Close connection
+    conn.close()
 
     # Clear the Text Boxes
     f_name.delete(0, END)
@@ -41,6 +59,36 @@ def submit():
     city.delete(0, END)
     state.delete(0, END)
     zipcode.delete(0, END)
+
+# Create a query function
+
+
+def query():
+    # Connect to the database
+    conn = sqlite3.connect('address_book.db')
+    # Create cursor for function
+    curs = conn.cursor()
+
+    # Query the database
+    curs.execute("SELECT *, oid FROM addresses")
+    records = curs.fetchall()
+    print(records)
+
+    # Loop through results
+    print_records = ''
+    for record in records:
+        # record[0] would return first names
+        print_records += str(record) + "\n"
+
+    query_label = Label(root, text=print_records)
+    query_label.grid(row=8, column=0, columnspan=2)
+
+    # Commit changes: after changes you want to commit
+    conn.commit()
+
+    # Close connection
+    conn.close()
+
 
 # Create text boxes
 f_name = Entry(root, width=30)
@@ -74,6 +122,9 @@ zipcode_label.grid(row=5, column=0)
 submit_btn = Button(root, text="Add Record to Database", command=submit)
 submit_btn.grid(row=6, column=0, columnspan=2, pady=10, padx=10, ipadx=100)
 
+# Create a Query Buttonn
+query_button = Button(root, text="Show Records", command=query)
+query_button.grid(row=7, column=0, columnspan=2, pady=10, padx=10, ipadx=137)
 
 # Commit changes: after changes you want to commit
 conn.commit()
