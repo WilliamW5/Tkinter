2a2d34a6e45542607eadf1d35cf3157bebec9238
WilliamW5 <WilliamWiemann@outlook.com>
2021-07-16 23:59:31 -0400

    testing

--- buttons.py
+++ buttons.py
@@ -1,12 +1,16 @@
 from tkinter import *
 
 root = Tk()
+# add
+
 
 def myClick():
     myLabel = Label(root, text="Look, I clicked a Button!")
     myLabel.pack()
 
-myButton = Button(root, text="Click Me!",command=myClick, fg="blue", bg="#000000")
+
+myButton = Button(root, text="Click Me!", command=myClick,
+                  fg="blue", bg="#000000")
 
 myButton.pack()
 
